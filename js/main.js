58dfa6d48c6451134b355962be9680a594cfa9e7
zhura <zhuravlevkos9@gmail.com>
2024-01-24 19:46:21 +0700

    Функционал третьего столбца

--- js/main.js
+++ js/main.js
@@ -12,12 +12,23 @@ Vue.component('kanban-card', {
                 <button @click="deleteCard">Delete</button>
                 <button v-if="columnIndex === 0" @click="moveToInProgress">Move to 'In Progress'</button>
                 <button v-if="columnIndex === 1" @click="moveToTesting">Move to 'Testing'</button>
+                <button v-if="columnIndex === 2" @click="moveToDone">Move to 'Done'</button>
+                <button v-if="columnIndex === 2" @click="returnToInProgress">Return to 'In Progress'</button>
             </div>
         </div>
     `,
     methods: {
         editCard() {
-            this.$emit('edit-card', this.columnIndex, this.cardIndex);
+            const updatedTitle = prompt('Enter new title', this.card.title);
+            const updatedDescription = prompt('Enter new description', this.card.description);
+            const updatedDeadline = prompt('Enter new deadline', this.card.deadline);
+
+            if (updatedTitle !== null && updatedDescription !== null && updatedDeadline !== null) {
+                this.card.title = updatedTitle;
+                this.card.description = updatedDescription;
+                this.card.deadline = updatedDeadline;
+                this.card.lastEdited = new Date().toLocaleString();
+            }
         },
         deleteCard() {
             this.$emit('delete-card', this.columnIndex, this.cardIndex);
@@ -27,6 +38,27 @@ Vue.component('kanban-card', {
         },
         moveToTesting() {
             this.$emit('move-to-testing', this.card, this.columnIndex, this.cardIndex);
+        },
+        moveToDone() {
+            this.$emit('move-to-done', this.card, this.columnIndex, this.cardIndex);
+        },
+        returnToInProgress() {
+            const inProgressIndex = 1;
+
+            // Проверяем, есть ли уже такая карточка во втором столбце
+            const isCardAlreadyInInProgress = this.$parent.columns[inProgressIndex].cards.some(card => card.title === this.card.title);
+
+            if (!isCardAlreadyInInProgress) {
+                this.$parent.columns[inProgressIndex].cards.push({
+                    title: this.card.title,
+                    description: this.card.description,
+                    deadline: this.card.deadline,
+                    dateCreated: this.card.dateCreated,
+                    lastEdited: new Date().toLocaleString()
+                });
+            }
+
+            this.$parent.columns[this.columnIndex].cards.splice(this.cardIndex, 1);
         }
     }
 });
@@ -64,19 +96,6 @@ new Vue({
         clearNewCard() {
             this.newCard = { title: '', description: '', deadline: '' };
         },
-        editCard(columnIndex, cardIndex) {
-            const card = this.columns[columnIndex].cards[cardIndex];
-            const updatedTitle = prompt('Enter new title', card.title);
-            const updatedDescription = prompt('Enter new description', card.description);
-            const updatedDeadline = prompt('Enter new deadline', card.deadline);
-
-            if (updatedTitle !== null && updatedDescription !== null && updatedDeadline !== null) {
-                card.title = updatedTitle;
-                card.description = updatedDescription;
-                card.deadline = updatedDeadline;
-                card.lastEdited = new Date().toLocaleString();
-            }
-        },
         deleteCard(columnIndex, cardIndex) {
             const confirmation = confirm('Are you sure you want to delete this card?');
             if (confirmation) {
@@ -106,6 +125,37 @@ new Vue({
                 dateCreated: originalCard.dateCreated,
                 lastEdited: originalCard.lastEdited
             });
+
+            this.columns[columnIndex].cards.splice(cardIndex, 1);
+        },
+        moveToDone(originalCard, columnIndex, cardIndex) {
+            const doneIndex = 3;
+
+            this.columns[doneIndex].cards.push({
+                title: originalCard.title,
+                description: originalCard.description,
+                deadline: originalCard.deadline,
+                dateCreated: originalCard.dateCreated,
+                lastEdited: originalCard.lastEdited
+            });
+
+            this.columns[columnIndex].cards.splice(cardIndex, 1);
+        },
+        returnToInProgress(originalCard, columnIndex, cardIndex) {
+            const inProgressIndex = 1;
+
+            // Проверяем, есть ли уже такая карточка во втором столбце
+            const isCardAlreadyInInProgress = this.columns[inProgressIndex].cards.some(card => card.title === originalCard.title);
+
+            if (!isCardAlreadyInInProgress) {
+                this.columns[inProgressIndex].cards.push({
+                    title: originalCard.title,
+                    description: originalCard.description,
+                    deadline: originalCard.deadline,
+                    dateCreated: originalCard.dateCreated,
+                    lastEdited: new Date().toLocaleString()
+                });
+            }
 
             this.columns[columnIndex].cards.splice(cardIndex, 1);
         }
